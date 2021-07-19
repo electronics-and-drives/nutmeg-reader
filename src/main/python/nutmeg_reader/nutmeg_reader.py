efb7601028711dbc5aca0817d003a2ef89eac1cf
Yannick Uhlmann <augustunderground@protonmail.com>
2021-07-19 12:46:38 +0200

    parsing in separate plot type

--- src/main/python/nutmeg_reader/nutmeg_reader.py
+++ src/main/python/nutmeg_reader/nutmeg_reader.py
@@ -6,6 +6,7 @@ import jpype.imports
 from jpype.types import *
 
 import numpy as np
+import pandas as pd
 
 if 'MAVEN_HOME' in os.environ:
     MAVEN_HOME = os.environ['MAVEN_HOME']
@@ -15,33 +16,55 @@ else:
 VERSION = '0.0.1'
 CLASS_PATH = f'{MAVEN_HOME}/edlab/eda/reader/nutmeg/{VERSION}/nutmeg-{VERSION}-jar-with-dependencies.jar'
 
+class NutmegPlot:
+    def __init__(self, plot):
+        self.plot         = plot
+        self.name         = plot.getPlotname()
+        self.num_of_waves = plot.getNoOfWaves()
+        self.num_of_point = plot.getNoOfPoints()
+
+        self.wave_names = [ wav.toString() 
+                            for wav in plot.getWaves().toArray() ]
+        self.wave_units = { wn: self.plot.getUnit(wn)
+                            for wn in self.wave_names }
+
+        self.wave_data  = pd.DataFrame({ wn: np.array([ w.getReal() + 1j * w.getImaginary() 
+                                                        for w in wf ]) \
+                                         if 'complex' in wf.toString() else \
+                                            np.array(wf)
+                                         for wn, wf in [ (w, plot.getWave(w)) 
+                                                         for w in self.wave_names ] })
+
 class NutmegReader:
     def __init__(self, class_path=CLASS_PATH):
         jpype.startJVM(classpath=[class_path])
         from edlab.eda.reader.nutmeg import NutReader
+        self.nut_reader = NutReader
 
-    def __readNutmeg(self, file_name, nut_type):
+    def __read_nutmeg(self, file_name, nut_type):
         '''
         Private reader function for handling ascii and bin types.
         See documentation for `readNutascii` and/or `readNutbin`.
         '''
-        if os.path.isfile(file_name):
-            
+        if os.path.isfile(file_name):   # Check if file exists
             if nut_type == 'ascii':
-                nutReader = NutReader.getNutasciiReader(file_name)
-            else:
-                nutReader = NutReader.getNutbinReader(file_name)
+                reader = self.nut_reader.getNutasciiReader(file_name)
+            else:                       # Raise error of file doesn't exist
+                reader = self.nut_reader.getNutbinReader(file_name)
 
-            nutReader.read()
-            nutReader.parse()
-            plots = nutReader.getPlots()
+            # Read and parse the file
+            reader.read()
+            reader.parse()
+
+            # Extract the plots as array
+            plots = [NutmegPlot(plot) for plot in reader.getPlots().toArray()]
         else:
             raise FileNotFoundError( errno.ENOENT
                                    , os.strerror(errno.ENOENT)
                                    , file_name)
         return plots
 
-    def readNutascii(self, file_name: str):
+    def read_nutascii(self, file_name: str):
         ''' 
         readNutascii : Reads the contents of a Nutmeg file in ascii syntax.
 
@@ -59,23 +82,23 @@ class NutmegReader:
             numOfPoints = plot1.num_of_point    references the number of points
 
         The name of a waveform can be accessed with:
-            name_1 = plot1.waveNames[0]     retrives the name of the first waveform
-            name_2 = plot1.waveNames[1]     retrives the name of the second waveform
+            name_1 = plot1.wave_names[0]        retrives the name of the first waveform
+            name_2 = plot1.wave_names[1]        retrives the name of the second waveform
 
         The unit of a waveform can be accessed with:
-            unit_1 = plot1.waveUnits[0]     retrives the unit of the first waveform 
-            unit_2 = plot1.waveUnits[1]     retrives the unit of the second waveform
+            unit_1 = plot1.wave_units(name_1)   retrives the unit of the first waveform 
+            unit_2 = plot1.wave_units(name_2)   retrives the unit of the second waveform
 
         The wave of a waveform can be accessed with:
-            wave_1 = plot1.waveData[:,0]  retrives the wave of the first waveform
-            wave_2 = plot1.waveData[:,1]  retrives the wave of the second waveform
+            wave_1 = plot1.wave_data(wave_1)    retrives the wave of the first waveform 
+            wave_2 = plot1.wave_data(name_2)    retrives the wave of the second waveform
          
         The entry plot1.waveData has the shape  
-            (plot1.numOfPoints, plot1.numOfWaves)
+            (plot1.num_of_points, plot1.num_of_waves)
         '''
         return self.__readNutmeg(file_name, 'ascii')
 
-    def readNutbin(self, file_name: str):
+    def read_nutbin(self, file_name: str):
         ''' 
         readNutbin : Reads the contents of a Nutmeg file in binary syntax.
 
