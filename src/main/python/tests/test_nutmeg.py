e303d573204bf0915bdf7f7eeb39fe516404f3bb
Yannick Uhlmann <augustunderground@protonmail.com>
2021-07-19 13:50:56 +0200

    simple tests

--- src/main/python/tests/test_nutmeg.py
+++ src/main/python/tests/test_nutmeg.py
@@ -2,10 +2,12 @@ from nutmeg_reader import NutmegReader
 
 nutreader = NutmegReader()
 
-
 def test_nutbin():
     bin_plots = nutreader.read_nutbin('../../../test/resources/rc2/nutbin.raw')
     assert(len(bin_plots) == 4)
 
-#def test_ascii():
-    #asc_plots = nutreader.read_nutbin('../../../test/resources/rc2/nutascii.raw')
+def test_ascii():
+    asc_plots = nutreader.read_nutascii('../../../test/resources/rc2/nutascii.raw')
+    assert(len(bin_plots) == 4)
+
+
