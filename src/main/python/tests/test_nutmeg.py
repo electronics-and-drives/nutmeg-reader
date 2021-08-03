2d30b52be9aa16ae49d58abb876e44d2cbcc4e94
Yannick Uhlmann <augustunderground@protonmail.com>
2021-08-03 11:26:44 +0200

    update tests

--- src/main/python/tests/test_nutmeg.py
+++ src/main/python/tests/test_nutmeg.py
@@ -1,33 +1,37 @@
-from nutmeg_reader import NutmegReader
-
-nutreader = NutmegReader()
+from nutmeg_reader import NutReader
 
 def test_nutbin():
-    bin_plots = nutreader.read_nutbin('../../../test/resources/rc2/nutbin.raw')
+    bin_file    = '../../../test/resources/rc2/nutbin.raw'
+    bin_reader  = NutReader.getNutbinReader(bin_file)
+    bin_reader.read().parse();
+    bin_plots   = bin_reader.getPlots().toArray()
 
     assert(len(bin_plots) == 4)
 
-    assert(bin_plots[0].num_of_points == 1)
-    assert(bin_plots[1].num_of_points == 51)
-    assert(bin_plots[2].num_of_points == 51)
-    assert(bin_plots[3].num_of_points == 56)
+    assert(bin_plots[0].getNoOfPoints == 1)
+    assert(bin_plots[1].getNoOfPoints == 51)
+    assert(bin_plots[2].getNoOfPoints == 51)
+    assert(bin_plots[3].getNoOfPoints == 56)
 
-    assert(bin_plots[0].num_of_waves == 6)
-    assert(bin_plots[1].num_of_waves == 6)
-    assert(bin_plots[2].num_of_waves == 5)
-    assert(bin_plots[3].num_of_waves == 6)
+    assert(bin_plots[0].getNoOfWaves == 6)
+    assert(bin_plots[1].getNoOfWaves == 6)
+    assert(bin_plots[2].getNoOfWaves == 5)
+    assert(bin_plots[3].getNoOfWaves == 6)
 
 def test_ascii():
-    asc_plots = nutreader.read_nutascii('../../../test/resources/rc2/nutascii.raw')
+    asc_file    = '../../../test/resources/rc2/nutascii.raw'
+    asc_reader  = NutReader.getNutasciiReader(asc_file)
+    asc_reader.read().parse();
+    asc_plots   = asc_reader.getPlots().toArray()
 
     assert(len(asc_plots) == 4)
 
-    assert(asc_plots[0].num_of_points == 1)
-    assert(asc_plots[1].num_of_points == 51)
-    assert(asc_plots[2].num_of_points == 51)
-    assert(asc_plots[3].num_of_points == 56)
+    assert(asc_plots[0].getNoOfPoints == 1)
+    assert(asc_plots[1].getNoOfPoints == 51)
+    assert(asc_plots[2].getNoOfPoints == 51)
+    assert(asc_plots[3].getNoOfPoints == 56)
 
-    assert(asc_plots[0].num_of_waves == 6)
-    assert(asc_plots[1].num_of_waves == 6)
-    assert(asc_plots[2].num_of_waves == 5)
-    assert(asc_plots[3].num_of_waves == 6)
+    assert(asc_plots[0].getNoOfWaves == 6)
+    assert(asc_plots[1].getNoOfWaves == 6)
+    assert(asc_plots[2].getNoOfWaves == 5)
+    assert(asc_plots[3].getNoOfWaves == 6)
