a431d5f41a37076f37004011ee5bd9de19bb3ef8
Yannick Uhlmann <augustunderground@protonmail.com>
2021-08-03 11:22:42 +0200

    show both ways of reading nutmegs

--- src/main/python/tests/demo_nutmeg.py
+++ src/main/python/tests/demo_nutmeg.py
@@ -1,18 +1,35 @@
-from nutmeg_reader import NutmegReader
 import pandas as pd
 from matplotlib import pyplot as plt
+from nutmeg_reader import NutReader, read_nutmeg
 
-nutreader = NutmegReader()
+## The slightly longer and painful way with all the information:
 
-bin_plots = nutreader.read_nutbin('../../../test/resources/rc2/nutbin.raw')
+nut_file    = '../../../test/resources/rc2/nutascii.raw'
+nut_reader  = NutReader.getNutasciiReader(nut_file)
 
-tran_plot = bin_plots[3]
+reader.read().parse();
 
-tran_data = pd.DataFrame(tran_plot.wave_data)
+plots       = reader.getPlots().toArray()
+tran_plot   = plots[3]
+
+tran_data   = pd.DataFrame({ w: tran_plot.getWave(w) 
+                             for w in tran_plot.getWaves().toArray() })
 
 fig, axs = plt.subplots(1,1, figsize=(8,8))
 axs.plot(tran_data['time'], tran_data['O'])
-axs.set_title(tran_plot.name)
-axs.set_xlabel(f'time ({tran_plot.wave_units["time"]})')
-axs.set_ylabel(f'O ({tran_plot.wave_units["O"]})')
+axs.set_title(tran_plot.getPlotname())
+axs.set_xlabel(f'time ({tran_plot.getUnit("time")})')
+axs.set_ylabel(f'O ({tran_plot.getUnit("O")})')
+plt.show()
+
+## Or with the `read_nutmeg` convenience function, but less information:
+
+bin_plots = read_nutmeg('../../../test/resources/rc2/nutbin.raw')
+tran_data = bin_plots['tran']
+
+fig, axs = plt.subplots(1,1, figsize=(8,8))
+axs.plot(tran_data['time'], tran_data['O'])
+axs.set_title('tran')
+axs.set_xlabel(f'time (s)')
+axs.set_ylabel(f'O (V)')
 plt.show()
