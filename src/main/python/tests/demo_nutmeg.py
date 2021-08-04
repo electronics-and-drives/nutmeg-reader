237fb5ba69ac2ce4334fb0b67595b80150cac0d1
Yannick Uhlmann <augustunderground@protonmail.com>
2021-08-04 11:01:58 +0200

    demo with jpype

--- src/main/python/tests/demo_nutmeg.py
+++ src/main/python/tests/demo_nutmeg.py
@@ -4,16 +4,16 @@ from nutmeg_reader import NutReader, read_nutmeg
 
 ## The slightly longer and painful way with all the information:
 
-nut_file    = '../../../test/resources/rc2/nutascii.raw'
-nut_reader  = NutReader.getNutasciiReader(nut_file)
+file    = '../../../test/resources/rc2/nutascii.raw'
+reader  = NutReader.getNutasciiReader(file)
 
 reader.read().parse();
 
-plots       = reader.getPlots().toArray()
+plots       = reader.getPlots()
 tran_plot   = plots[3]
 
-tran_data   = pd.DataFrame({ w: tran_plot.getWave(w) 
-                             for w in tran_plot.getWaves().toArray() })
+tran_data   = pd.DataFrame({ w: list(tran_plot.getWave(w))
+                             for w in list(tran_plot.getWaves()) })
 
 fig, axs = plt.subplots(1,1, figsize=(8,8))
 axs.plot(tran_data['time'], tran_data['O'])
